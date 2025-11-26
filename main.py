cc66db73db33730ff9f225b101c75bc15150d2cd
AndreiGiocas <andrei_giocas@yahoo.com>
2025-11-26 12:59:30 +0200

    created player character

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 def main():
@@ -7,13 +8,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
